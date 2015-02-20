9708ef20c109cf047361d89b83e0ded6f67dce2e
Karin <Karin@yasudakarin-no-MacBook-Air.local>
2015-02-20 18:47:24 +0900

    first commit

--- ex_NSArray/ViewController.m
+++ ex_NSArray/ViewController.m
@@ -9,8 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+//運勢ボタンとアクション接続して挿入したメソッド宣言
+- (IBAction)choiceFortune:(UIButton *)sender;
+//運勢ラベルとアウトレット接続して挿入すたプロパティ
+@property (weak, nonatomic) IBOutlet UILabel *fortune;
 
 @end
+
+
+
 
 @implementation ViewController
 
@@ -24,4 +31,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+//運勢ボタンのタップで実行するメソッド
+- (IBAction)choiceFortune:(UIButton *)sender {
+    //配列を作る
+    NSArray *fortuneList=@[@"吉",@"吉",@"吉",@"大吉",@"凶",@"大凶"];
+    //配列から取り出す位置を乱数で決める
+    int pos = arc4random()% fortuneList.count;
+    //配列からストリングを取り出す
+    NSString *fortuneString =fortuneList[pos];
+    //取り出したストリングをラベルに表示する
+    self.fortune.text=fortuneString;
+}
 @end
